9f65184431de546cb3d3cb513936a2315f581e8b
CtrlZ233 <328825619@qq.com>
2025-03-19 17:27:12 +0800

    init repo

---
+++ src/device_tree.rs
@@ -0,0 +1,49 @@
+use log::warn;
+use fdt::Fdt;
+use lazy_init::LazyInit;
+
+static FDT: LazyInit<Fdt> = LazyInit::new();
+
+pub struct FDTParser;
+
+impl FDTParser {
+    pub fn get_memory_range(&self) -> Result<(usize, usize), ()> {
+        if !FDT.is_init() {
+            warn!("FDT is not initialized");
+            return Err(());
+        }
+        for node in FDT.all_nodes() {
+            if node.name.starts_with("memory@") {
+                if let Some(reg) = node.property("reg") {
+                    let reg_bytes = reg.value;
+                    // 检查字节切片的长度
+                    if reg_bytes.len() >= 16 {
+                        // 解析起始地址和大小（假设每个部分为 64 位）
+                        let start = u64::from_be_bytes([
+                            reg_bytes[0], reg_bytes[1], reg_bytes[2], reg_bytes[3],
+                            reg_bytes[4], reg_bytes[5], reg_bytes[6], reg_bytes[7],
+                        ]) as usize;
+
+                        let size = u64::from_be_bytes([
+                            reg_bytes[8], reg_bytes[9], reg_bytes[10], reg_bytes[11],
+                            reg_bytes[12], reg_bytes[13], reg_bytes[14], reg_bytes[15],
+                        ]) as usize;
+                        return Ok((start, size));
+                    }
+                }
+            }
+        }
+        Err(())
+    }
+}
+
+
+pub fn parse_dtb(dtb_paddr: usize) {
+    let dtb_ptr = (dtb_paddr) as *const u8;
+    if dtb_ptr.is_null() {
+        warn!("No valid DTB address provided!");
+        return;
+    }
+    let dtb = unsafe { Fdt::from_ptr(dtb_ptr).expect("Failed to parse DTB") };
+    FDT.init_by(dtb);
+}
---
+++ src/lib.rs
@@ -0,0 +1,41 @@
+#![no_std]
+
+use log::debug;
+use common::utils::alignas::{align_down, align_up};
+use crate::device_tree::FDTParser;
+
+pub mod console;
+mod logging;
+mod lang_items;
+mod device_tree;
+
+pub fn shutdown(failure: bool) -> ! {
+    mork_riscv::sbi::shutdown(failure)
+}
+
+pub fn console_putchar(c: char) {
+    mork_riscv::sbi::console_putchar(c as usize);
+}
+
+pub fn console_getchar() -> usize {
+    mork_riscv::sbi::console_getchar()
+}
+
+pub fn get_free_memory() -> Result<(usize, usize), ()> {
+    let (start, size) = FDTParser.get_memory_range()?;
+    unsafe extern "C" {
+        fn kernel_end();
+    }
+    Ok((align_up(kernel_end as usize, 4096), align_down(start + size, 4096)))
+}
+
+pub fn boot_init(dtb_paddr: usize) -> ! {
+    mork_riscv::clear_bss();
+
+    logging::init();
+
+    debug!("mork initialized: {:#x}", dtb_paddr);
+    device_tree::parse_dtb(dtb_paddr + mork_riscv::KERNEL_OFFSET);
+
+    shutdown(false)
+}
